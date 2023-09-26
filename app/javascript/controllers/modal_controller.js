cfcefb3c97f6a54135aa2ffc52315cee58376aa2
Eleonore Baillot <eleonorebaillot@gmail.com>
2023-09-26 12:17:49 +0200

    modal show for posts

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,15 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
+import { end } from "@popperjs/core";
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ['form'];
+  static targets = ['modal', 'less'];
   connect() {
-    console.log("coucouc");
-    console.log(this.formTarget)
   }
-  toggle() {
-    // this.modalTarget.classList.toggle('show');
-    this.formTarget.open();
-
+  open(event) {
+    event.preventDefault()
+    this.modalTarget.classList.toggle("hide");
+    if (this.modalTarget.classList.contains('hide')) {
+      this.lessTarget.innerText = "View more";
+    } else {
+      this.lessTarget.innerText = "View less"
+    }
   }
 }
